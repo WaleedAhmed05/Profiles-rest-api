7cc33cf038259a78c9811d8a6d5b18b4a05ed6d4
Waleed Ahmed <waleedclash05@gmail.com>
2021-03-21 17:08:55 +0500

    Added search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from profiles_api import serializers
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
+from rest_framework import filters
 
 
 class HelloApiView(APIView):
@@ -104,6 +105,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-    
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
